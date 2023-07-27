7a46e9e8a998a3f7bdf6fdd3aba6a3d271aab230
Tinaesh <tjegades@student.42singapore.sg>
2023-07-27 10:51:08 +0800

    ex06: done

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: tjegades <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:27:24 by tjegades          #+#    #+#             */
-/*   Updated: 2023/07/26 18:48:49 by tjegades         ###   ########.fr       */
+/*   Updated: 2023/07/27 10:47:53 by tjegades         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,33 @@
 
 void	ft_print_comb2(void)
 {
-	char	i;
-	char	j;
+	char	left_char[3];
+	char	right_char[2];
+	int		left_num;
+	int		right_num;
 
-	i = 48;
-	while (i < 57)
+	left_num = 0;
+	left_char[2] = ' ';
+	while (left_num < 99)
 	{
-		j = i + 1;
-		while (j <= 57)
+		left_char[0] = left_num / 10 + 48;
+		left_char[1] = left_num % 10 + 48;
+		right_num = left_num + 1;
+		while (right_num <= 99)
 		{
-			write(1, &i, 1);
-			write(1, " ", 1);
-			write(1, &j, 1);
-			write(1, ", ", 2);
-			j++;
+			right_char[0] = right_num / 10 + 48;
+			right_char[1] = right_num % 10 + 48;
+			write(1, left_char, 3);
+			write(1, right_char, 2);
+			if (left_num != 98)
+				write(1, ",", 1);
+			right_num++;
 		}
-		i++;
+		left_num++;
 	}
 }
-int	main(void)
-{
-	ft_print_comb2();
-}
+
+// int	main(void)
+// {
+// 	ft_print_comb2();
+// }
