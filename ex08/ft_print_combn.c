01f9178e424fe23787817f0ff3776e662402705e
Tinaesh <tjegades@student.42singapore.sg>
2023-07-28 14:11:05 +0800

    ex08 in 25 lines :)

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -12,51 +12,32 @@
 
 #include <unistd.h>
 
-void	initialise_array(char *arr, int size)
-{
-	int		i;
-
-	i = 9 - size;
-	while (i < 9)
-	{
-		arr[i] = 48;
-		i++;
-	}
-}
-
-void	increment_last_and_print(char *arr, int size, int pointer)
-{
-	arr[8]++;
-	write(1, arr + 9 - size, size);
-	if (!(pointer == 9 - size && arr[pointer] == 58 - size && arr[8] == '9'))
-		write(1, ", ", 2);
-}
-
 void	ft_print_combn(int n)
 {
 	char	comb[9];
 	int		i;
 	int		j;
 
-	initialise_array(comb, n);
+	i = 9 - n;
+	while (i++ < 9)
+		comb[i - 1] = 48;
 	i = 7;
-	while (i >= 9 - n)
+	while (i-- >= 9 - n)
 	{
-		while (comb[8] < 57)
-			increment_last_and_print(comb, n, i);
-		if (comb[i] < 58 - (9 - i))
+		while (comb[8]++ < 57)
 		{
-			comb[i]++;
-			j = i + 1;
-			while (j < 8)
-			{
-				comb[j] = comb[j - 1] + 1;
-				j++;
-			}
-			comb[8] = comb[7];
-			continue ;
+			write(1, comb + 9 - n, n);
+			if (!(i + 1 == 9 - n && comb[i + 1] == 58 - n && comb[8] == '9'))
+				write(1, ", ", 2);
 		}
-		i--;
+		if (comb[i + 1]++ < 58 - (8 - i))
+		{
+			j = i + 2;
+			while (j++ < 8)
+				comb[j - 1] = comb[j - 2] + 1;
+			comb[8] = comb[7];
+			i++;
+		}
 	}
 }
 /*int	main(void)
