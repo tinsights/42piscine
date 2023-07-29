b27aacf174ebe60c3a7f598933a9a27c46e6b7df
Tinaesh <tjegades@student.42singapore.sg>
2023-07-29 11:34:38 +0800

    ex10 works

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjegades <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 11:23:33 by tjegades          #+#    #+#             */
+/*   Updated: 2023/07/29 11:23:34 by tjegades         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i++ < size - 1)
+		dest[i - 1] = src[i - 1];
+	src[i] = '\0';
+	return i;
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	char	c[] = "hello";
+	char	d[100];
+	ft_strlcpy(d, c, 3);
+	printf("%s", d);
+}
