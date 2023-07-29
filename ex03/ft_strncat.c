1eed03cc4e085e668abe5beb273d7ee8e265b28a
Tinaesh <tjegades@student.42singapore.sg>
2023-07-29 10:43:59 +0800

    ex03 strncat

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -10,4 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	char			*original;
+	unsigned int	count;
 
+	count = 0;
+	original = dest;
+	while (*dest)
+		dest++;
+	while (*src && count++ < nb)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (original);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char dest[100] = "tell me whyyy";
+	char src[] = ", aint nothing but a heartache";
+	printf("%s\n", ft_strncat(dest, src, 1));
+}
+*/
