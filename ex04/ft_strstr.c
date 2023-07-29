622fe02364dfbe71f2655a63189a2f58e8a9a9b8
Tinaesh <tjegades@student.42singapore.sg>
2023-07-29 10:59:36 +0800

    ex04 strstr

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
 
+	while (*str && *str != *to_find)
+	{
+		i = 0;
+		while (str[i] && to_find[i] && str[i] == to_find[i])
+		{
+			if (to_find[i] == '\0')
+				return str;
+			i++;
+		}
+		str++;
+	}
+	return str;
+}
