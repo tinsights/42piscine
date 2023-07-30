48880c451cc721f2a1aef89a6985db57ff11427b
Tinaesh Jegadessan <tjegades@c2r2s12.42singapore.sg>
2023-07-30 17:27:58 +0800

    comments and cleanup for submission

--- ex00/main.c
+++ ex00/main.c
@@ -13,6 +13,16 @@
 void	rush(int x, int y);
 int		atoi(char *str);
 
+/**
+* Defining main like this allows
+* the compiled program to take in
+* input from the command line when run.
+* argc is an integer that contains the number of
+* commands typed into the terminal,
+* and argv is an array of strings, containing the commands typed in.
+* i.e. if the program is run with ./a.out 5 3,
+* then argc = 3, and argv = ["a.out", "5", "3"]
+**/
 int	main(int argc, char **argv)
 {
 	if (argc != 3)
@@ -35,3 +45,48 @@ int	atoi(char *str)
 	}
 	return (result);
 }
+
+/*
+* Defining main like this allows
+* the compiled program to take in
+* input from the command line when run.
+* argc is an integer that contains the number of
+* commands typed into the terminal,
+* and argv is an array of strings, containing the commands typed in.
+* i.e. if the program is run with ./a.out 5 3,
+* then argc = 3, and argv = ["a.out", "5", "3"]
+*
+int	main(int argc, char **argv)
+{	
+	// here we check that the right
+	// number of arguments were entered.
+	// if the user didn't input a length and width, the program ends.
+	if (argc != 3)
+		return (-1);
+	
+	// else, the rush function is called using the numbers passed in
+	rush (atoi(argv[1]), atoi(argv[2]));
+}
+
+* This function converts the string "5" into the integer 5
+* Because rush() expects input of type int, but main()
+* receives input from the terminal of type char*.
+* It does basic ASCII conversion: looks for digits between '0' and '9'
+* and subtracts the ASCII value to get back the integer digits.
+
+int	atoi(char *str)						
+{
+	int	i;
+	int	result;
+
+	i = 0;
+	result = 0;
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	return (result);
+}
+*/
--- ex00/rush02.c
+++ ex00/rush02.c
@@ -37,3 +37,126 @@ void	rush(int x, int y)
 		row++;
 	}
 }
+
+/***************************
+
+RUSH 00
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j <= y)
+	{
+		i = 1;
+		while (i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == x && j == 1)
+				|| (i == 1 && j == y) || (i == x && j == y))
+				ft_putchar('o');
+			else if ((i == x || i == 1))
+				ft_putchar('|');
+			else if (j == 1 || j == y)
+				ft_putchar('-');
+			else
+				ft_putchar(' ');
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+}
+
+**************************
+
+RUSH 01
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j <= y)
+	{
+		i = 1;
+		while (i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == x && j == y && y != 1 && x != 1))
+				ft_putchar('/');
+			else if ((i == 1 && j == y) || (i == x && j == 1))
+				ft_putchar('\\');
+			else if ((i == x || i == 1) || (j == 1 || j == y))
+				ft_putchar('*');
+			else
+				ft_putchar(' ');
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+}
+
+**************************
+
+RUSH 03
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j <= y)
+	{
+		i = 1;
+		while (i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == 1 && j == y))
+				ft_putchar('A');
+			else if ((i == x && j == 1) || (i == x && j == y))
+				ft_putchar('C');
+			else if ((i == x || i == 1) || (j == 1 || j == y))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+}
+
+**************************
+
+RUSH 04
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j <= y)
+	{
+		i = 1;
+		while (i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == x && j == y && y != 1 && x != 1))
+				ft_putchar('A');
+			else if ((i == 1 && j == y) || (i == x && j == 1))
+				ft_putchar('C');
+			else if ((i == x || i == 1) || (j == 1 || j == y))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+}
+
+*/
