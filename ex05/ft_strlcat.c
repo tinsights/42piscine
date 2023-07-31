63604fb29cf97a9b5a8750aa2ea888febf3a6f93
Tinaesh <tjegades@student.42singapore.sg>
2023-07-31 16:30:27 +0800

    ex05: fix

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	int				length;
 
+	length = 0;
+	while (*dest++)
+		length++;
+	dest -= 1;
+	i = 0;
+	while (src[i] != '\0' && i++ < size - length - 1)
+		dest[i - 1] = src[i - 1];
+	dest[i] = '\0';
+	return (i);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	c[] = "world. nice to meet you! happy to be here.";
+	char	d[20] = "hello ";
+	printf("%i\n", ft_strlcat(d, c, 20));
+	printf("%s", d);
+}
+*/
