07df734e4394fa389b3090ea9223e585c98175b0
Tinaesh <tjegades@student.42singapore.sg>
2023-08-02 08:36:42 +0800

    ex10: fix return

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -13,22 +13,31 @@
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
+	unsigned int	len;
 
+	len = 0;
+	while (src[len])
+		len++;
 	i = 0;
-	while (src[i] && i < size - 1)
+	if ((int)size > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
-	return (i);
+	return (len);
 }
 
-// #include <stdio.h>
-// int	main(void)
-// {
-// 	char	c[] = "hello";
-// 	char	d[10];
-// 	printf("%i\n", ft_strlcpy(c, c, 10000));
-// 	printf("%s", d);
-// }
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	c[] = "hello";
+	char	d[10] = "abcdefg";
+	printf("%i\n", ft_strlcpy(d, c, 10));
+	printf("%s", d);
+}
+*/
