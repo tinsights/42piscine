31ff96137fe1cb74f185304d3d3336bd8e04746e
Tinaesh <tjegades@student.42singapore.sg>
2023-08-04 10:52:56 +0800

    cleaned up 10 queens

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -17,30 +17,7 @@ void place_queen(int *board);
 
 int	ft_ten_queens_puzzle(void)
 {
-	int board[10][10];
 	int queens = 3;
-	
-	for (int i = 0; i < 10; i++)
-		for (int j = 0; j < 10; j++)
-			board[i][j] = 1;
 
-	for (int r = 0; r < 10; r++)
-	{
-		for (int c = 0; c < queens; c++)
-		{
-			place_queen(board[r]);
-			place_queen(board[c]);
-		}
-	}
 	return (0);
-}
-
-void place_queen(int *board)
-{
-
-}
-
-int	main(void)
-{
-	ft_ten_queens_puzzle();
 }
