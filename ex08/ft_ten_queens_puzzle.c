e9ca526a932c5feb57647483828410e95ca0ca76
Tinaesh <tjegades@student.42singapore.sg>
2023-08-08 13:03:18 +0800

    ten queens

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -13,11 +13,59 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void place_queen(int *board);
+int	valid(char *sequence, int curr_col)
+{
+	int		i;
+	char	curr_q;
+
+	i = -1;
+	curr_q = sequence[curr_col];
+	while (++i < curr_col)
+	{
+		if (curr_q == sequence[i]
+			|| curr_q == sequence[i] + (curr_col - i)
+			|| curr_q == sequence[i] - (curr_col - i))
+			return (0);
+	}
+	return (1);
+}
+
+int	set_and_check(char *sequence, int col)
+{
+	int		count;
+	char	row;
+
+	if (col == 10)
+	{
+		write(1, sequence, 10);
+		write(1, "\n", 1);
+		return (1);
+	}
+	count = 0;
+	row = '0';
+	while (row <= '9')
+	{
+		sequence[col] = row;
+		if (valid(sequence, col))
+		{
+			count += set_and_check(sequence, col + 1);
+		}
+		row++;
+	}
+	return (count);
+}
 
 int	ft_ten_queens_puzzle(void)
 {
-	int queens = 3;
+	char	sequence[10];
 
-	return (0);
+	return (set_and_check(sequence, 0));
 }
+
+/*
+int	main(void)
+{
+	printf("----------\n");
+	printf("%i\n" ,ft_ten_queens_puzzle());
+}
+*/
